023b6765373f4c9f36517bd4771152398a09abb3
Faruk Terzioğlu <faruk.terzioglu@hotmail.com>
2018-12-17 11:29:22 +0300

    Progress on kafka consumer

--- KafkaComparer.Consumer/Program.cs
+++ KafkaComparer.Consumer/Program.cs
@@ -1,42 +1,81 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Confluent.Kafka;
-using Confluent.Kafka.Serialization;
 
 namespace KafkaComparer.Consumer
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      var consumergroup = Environment.GetEnvironmentVariable("CONSUMER_GROUP");
-      var topicName = Environment.GetEnvironmentVariable("TOPIC_NAME");
-      var kafkaUrl = Environment.GetEnvironmentVariable("KAFKA_URL");
+        static void Main(string[] args)
+        {
+            var consumergroup = Environment.GetEnvironmentVariable("CONSUMER_GROUP");
+            if (string.IsNullOrEmpty(consumergroup)) 
+                consumergroup = Guid.NewGuid().ToString();
 
-      var config = new Dictionary<string, object>
+            var topicName = Environment.GetEnvironmentVariable("TOPIC_NAME");
+            var brokerList = Environment.GetEnvironmentVariable("KAFKA_URL");
+
+            Offset offset;
+            var offSetEnvVar = Environment.GetEnvironmentVariable("OFFSET");
+            if(string.IsNullOrEmpty(offSetEnvVar) && long.TryParse(offSetEnvVar, out long offsetValue)){
+                offset = new Offset(offsetValue);
+            } else {
+                offset = Offset.End;
+            }
+
+            // https://kafka.apache.org/0110/documentation.html#newconsumerconfigs
+            var config = new ConsumerConfig
             {
-                { "group.id", consumergroup },
-                { "bootstrap.servers", kafkaUrl },
-                { "enable.auto.commit", "false"}
+                GroupId = consumergroup,
+                BootstrapServers = brokerList,
+                // Note: The AutoOffsetReset property determines the start offset in the event
+                // there are not yet any committed offsets for the consumer group for the
+                // topic/partitions of interest. By default, offsets are committed
+                // automatically, so in this example, consumption will only start from the
+                // eariest message in the topic 'my-topic' the first time you run the program.
+                //AutoOffsetReset = AutoOffsetResetType.Earliest,
+                //MessageMaxBytes = 3000000
             };
 
-      using (var consumer = new Consumer<Null, string>(config, null, new StringDeserializer(Encoding.UTF8)))
-      {
-        consumer.Subscribe(new string[] { topicName });
+            using (var consumer = new Consumer<Null, string>(config))
+            {
+                consumer.OnError += (_, e) => Console.WriteLine($"Error: {e.Reason}");
 
-        consumer.OnMessage += (_, msg) =>
-        {
-          Console.WriteLine($"Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
-          consumer.CommitAsync(msg);
-        };
+                bool consuming = true;
+                // The client will automatically recover from non-fatal errors. You typically
+                // don't need to take any action unless an error is marked as fatal.
+                consumer.OnError += (_, e) => consuming = !e.IsFatal;
 
-        while (true)
-        {
-          consumer.Poll(100);
+                consumer.Assign(new List<TopicPartitionOffset>() { new TopicPartitionOffset(topicName, partition: 0, offset: Offset.Beginning) });
+
+                // You can use this event to perform actions such as retrieving offsets
+                // from an external source / manually setting start offsets using
+                // the Assign method.
+                //consumer.OnPartitionsAssigned += (obj, partitions) => {
+                //    Console.WriteLine($"Assigned partitions: [{string.Join(", ", partitions)}], member id: {consumer.MemberId}");
+                //    var beginning = partitions.Select(p => new TopicPartitionOffset(p.Topic, p.Partition, offset)).ToList();
+                //    Console.WriteLine($"Updated assignment: [{string.Join(", ", beginning)}]");
+                //    consumer.Assign(beginning);
+                //};
+
+                consumer.Subscribe(new string[] {topicName});
+                while (consuming)
+                {
+                    try
+                    {
+                        var msg = consumer.Consume();
+                        Console.WriteLine($"{msg.Value}\nTopic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} TopicPartitionOffset: { msg.TopicPartitionOffset} \n");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occured: {e.Error.Reason}");
+                    }
+                }
+                consumer.Close();
+            }
         }
-      }
     }
-  }
 }
--- KafkaComparer.Producer/Program.cs
+++ KafkaComparer.Producer/Program.cs
@@ -2,34 +2,41 @@
 using System.Collections.Generic;
 using System.Text;
 using Confluent.Kafka;
-using Confluent.Kafka.Serialization;
 
 namespace KafkaComparer.Producer
 {
-  public class Program
-  {
-    static void Main(string[] args)
+    public class Program
     {
-      var topicName = Environment.GetEnvironmentVariable("TOPIC_NAME");
-      var kafkaUrl = Environment.GetEnvironmentVariable("KAFKA_URL");
-
-      var config = new Dictionary<string, object>
-      {
-				{ "bootstrap.servers", kafkaUrl }
-			};
-
-      using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
-      {
-        string text = null;
-
-        while (text != "exit")
+        static void Main(string[] args)
         {
-          text = Console.ReadLine();
-          producer.ProduceAsync(topicName, null, text);
-        }
+            var topicName = Environment.GetEnvironmentVariable("TOPIC_NAME");
+            var kafkaUrl = Environment.GetEnvironmentVariable("KAFKA_URL");
 
-        producer.Flush(100);
-      }
+            // https://kafka.apache.org/0110/documentation.html#producerconfigs
+            var config = new ProducerConfig(){
+                //MessageMaxBytes = 3000000,
+                BootstrapServers = kafkaUrl
+            };
+
+            using (var producer = new Producer<Null, string>(config))
+            {
+                string text = null;
+
+                while (text != "exit")
+                {
+                    text = Console.ReadLine();
+                    try
+                    {
+                        var dr = producer.ProduceAsync(topicName, new Message<Null, string> { Value = text }).GetAwaiter().GetResult();
+                        Console.WriteLine($"{DateTime.Now} Delivered to '{dr.TopicPartitionOffset}'");
+                    }
+                    catch (KafkaException e)
+                    {
+                        Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                    }
+                }
+                producer.Flush(TimeSpan.FromSeconds(10));
+            }
+        }
     }
-  }
 }
